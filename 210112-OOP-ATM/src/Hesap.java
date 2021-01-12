419516fa0474395e526edd1701e7a05ed8c5068b
MFE <baymfe@gmail.com>
2021-01-12 22:49:34 +0300

    Update Hesap

--- 210112-OOP-ATM/src/Hesap.java
+++ 210112-OOP-ATM/src/Hesap.java
@@ -3,4 +3,47 @@ public class Hesap {
 	/*
 	 * Hesap bilgileri tutulacak
 	 */
+	private int musteriNo, sifre, bakiye;
+	private String isim;
+
+	public Hesap(int musteriNo, int sifre, int bakiye, String isim) {
+		this.musteriNo = musteriNo;
+		this.sifre = sifre;
+		this.bakiye = bakiye;
+		this.isim = isim;
+	}
+
+	public int getMusteriNo() {
+		return musteriNo;
+	}
+
+	public void setMusteriNo(int musteriNo) {
+		this.musteriNo = musteriNo;
+	}
+
+	public int getSifre() {
+		return sifre;
+	}
+
+	public void setSifre(int sifre) {
+		this.sifre = sifre;
+	}
+
+	public int getBakiye() {
+		return bakiye;
+	}
+
+	public void setBakiye(int bakiye) {
+		this.bakiye = bakiye;
+	}
+
+	public String getIsim() {
+		return isim;
+	}
+
+	public void setIsim(String isim) {
+		this.isim = isim;
+	}
+	
+
 }
