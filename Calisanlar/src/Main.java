269b22adc1673ca5b57a5f57f83c227e776f4458
MFE <baymfe@gmail.com>
2021-01-15 22:18:54 +0300

    Main
    Ana akış yazılması gerek !

---
+++ Calisanlar/src/Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
+/*
+ * ------- Main
+ * YAPILACAAAAAAAAAAK
+ * 
+ * 
+ * ------- Çalýþanlar
+ * Ana sýnýf, diðerlerine kaltým yoluyla aktaracaðýz.
+ * +Deðiþkenler: isim,soyisim,id
+ * +Metotlar: bilgileri göster(diðerleri override)
+ * 
+ * ------- Yazýlýmcý
+ * kalýtým-çalýþanlar
+ * +Deðiþkenler: bildiði diller
+ * +Metotlar: format at, bilgileri göster(override-diller)
+ * 
+ * ------- Yönetici
+ * kalýtým-çalýþanlar
+ * +Deðiþkenler: sorumlu olduðu kiþi sayýsý
+ * +Metotlar:Zam yap
+ * 
+ */
